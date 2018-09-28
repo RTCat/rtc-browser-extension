8679775812ea33c902fba81d64481edc5e6715ee
Yi Kai <beliefs111@gmail.com>
2018-09-28 16:09:30 +0800

    add tab source

--- chrome/background.js
+++ chrome/background.js
@@ -7,7 +7,7 @@ chrome.runtime.onMessageExternal.addListener(
       sendResponse({version: chrome.runtime.getManifest().version})
     } else if (request.getStream) {
       chrome.desktopCapture.chooseDesktopMedia(
-        ['screen', 'window'], sender.tab,
+        ['window', 'screen', 'tab'], sender.tab,
         function (sourceId) {
           if (!sourceId || !sourceId.length) {
             sendResponse({error: 'Permission denied'})
@@ -47,7 +47,7 @@ function requestScreenSharing (port, msg) {
   //  - 'streamId' String that can be passed to getUserMedia() API
   // Also available:
   //  ['screen', 'window', 'tab', 'audio']
-  const sources = ['screen', 'window']
+  const sources = ['window', 'screen', 'tab']
   const tab = port.sender.tab
 
   desktopMediaRequestId = chrome.desktopCapture.chooseDesktopMedia(
